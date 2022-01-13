1b2ecdd15ea802c7df9d9a7c1d3ac6bf9bef9dd5
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-12 19:25:02 -0600

    implemented first material UI item, will start working on it later

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Weather from "../components/Weather"
+import Weather from "../components/Weather";
+import Button from '@mui/material/Button';
+
 
 const Home = () => {
   return (
     <div className="container">
         <h1> Placeholder header</h1>
+        <Button variant="contained">test button</Button>
       <Weather />
     </div>
   );
 };
 
 export default Home;
+
