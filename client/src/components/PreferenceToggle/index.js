1632f4dcf3f2f20d52eb76b5e14777cbd53cf31e
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-13 21:04:33 -0600

    finalized toggle look

--- client/src/components/PreferenceToggle/index.js
+++ client/src/components/PreferenceToggle/index.js
@@ -1,15 +1,42 @@
 import * as React from "react";
-import { alpha, styled } from "@mui/material/styles";
-import { pink } from "@mui/material/colors";
 import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
+// THE FORM GROUP SHOULD BE IN THE CONTAINER, AND FORMCONTROLLABEL SHOULD BE THE PREFERENCETOGGLE COMPONENT ITSELF
 
-const label = { inputProps: { "aria-label": "Switch" } };
-
-export default function ColorSwitches() {
+export default function FormControlLabelPosition() {
     return (
-        <div>
-            <Switch {...label} defaultUnchecked />
-        </div>
+        <FormControl component="fieldset">
+            <FormLabel component="legend"></FormLabel>
+            <FormGroup aria-label="position" >
+                <FormControlLabel
+                    value="start"
+                    control={<Switch color="primary" />}
+                    label="Temperature"
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    value="start"
+                    control={<Switch color="primary" />}
+                    label="Humidity"
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    value="start"
+                    control={<Switch color="primary" />}
+                    label="Visibilty"
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    value="start"
+                    control={<Switch color="primary" />}
+                    label="Air Quality"
+                    labelPlacement="start"
+                />
+            </FormGroup>
+        </FormControl>
     );
 }
