75ba6e2c31c04efe3b7206138f2e08b91f1c81af
kboston91 <kourtneygboston@gmail.com>
2022-01-15 17:30:07 -0600

    added card information and dummy data

--- client/src/components/WeatherCards/index.js
+++ client/src/components/WeatherCards/index.js
@@ -1,90 +1,48 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import weatherData from 'src/weather.json';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
-export default function WeatherCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+function WeatherCard(props) {
+  const [weatherList, setWeatherList] = useState(weatherData);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        // avatar={
-        //   <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-        //     R
-        //   </Avatar>
-        // }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
         title="Today's Weather"
-        subheader="Actual Date"
-      />
+        subheader="Actual Date:"
+      /> {props.date}
       <CardMedia
         component="img"
         height="194"
         //weather gif from canva below
         image="image file"
         alt="Weather GIF"
+        {...props.image}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          Weather Data
+          <ul>
+          <li>
+            <strong>Temperature:</strong> {props.temp}
+          </li>
+          <li>
+            <strong>Humidity:</strong> {props.humidity}
+          </li>
+          <li>
+            <strong>UVI:</strong> {props.uvi}
+          </li>
+          <li>
+            <strong>Feels Like:</strong> {props.feelsLike}
+          </li>
+        </ul>
         </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>More Info:</Typography>
-          <Typography paragraph>
-            TBD DATA
-          </Typography>
         </CardContent>
-      </Collapse>
     </Card>
   );
 }
+export default WeatherCard;
