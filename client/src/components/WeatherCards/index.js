7e6c3e014d170d8bf8fa270eeab170d03c7c6b4d
kboston91 <kourtneygboston@gmail.com>
2022-01-16 09:15:52 -0600

    correcting paths in weatercards file

--- client/src/components/WeatherCards/index.js
+++ client/src/components/WeatherCards/index.js
@@ -1,11 +1,30 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import weatherData from 'src/weather.json';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function WeatherCard(props) {
   const { data } = props;
@@ -40,7 +59,6 @@ export default function WeatherCard(props) {
         //weather gif from canva below
         image={URL_icon}
         alt="Weather GIF"
-        {...props.image}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
@@ -49,8 +67,31 @@ export default function WeatherCard(props) {
           <div>Visibility: {data.visibility} </div>
           <div>Wind speed: {data.wind.speed} m/s</div>
         </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>More Info:</Typography>
+          <Typography paragraph>
+            TBD DATA
+          </Typography>
         </CardContent>
+      </Collapse>
     </Card>
   );
 }
-export default WeatherCard;
