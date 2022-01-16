42ca0675aa6a25073cdb45b794f6b6b93e8a9273
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-16 14:13:40 -0600

    cleaned up the code a bit

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -22,7 +20,7 @@ function Copyright(props) {
             {...props}
         >
             {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
+            <Link color="inherit" href="/">
                 Gale
             </Link>{" "}
             {new Date().getFullYear()}
