9722e21bec64b39c790086ec5eb3187d4acf44dc
Ernesto Valecillos <evalecillos@gmail.com>
2022-01-16 13:26:12 -0600

    removed first name and last name from signup, changed to username

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -69,25 +69,14 @@ export default function SignUp() {
                         sx={{ mt: 3 }}
                     >
                         <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="firstName"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
+                            <Grid item xs={12}>
                                 <TextField
                                     required
                                     fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="family-name"
+                                    id="username"
+                                    label="Username"
+                                    name="username"
+                                    autoComplete="username"
                                 />
                             </Grid>
                             <Grid item xs={12}>
