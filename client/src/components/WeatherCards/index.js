88cc3ef31e74367525e8a544c0dcbd5139d6e95d
kboston91 <kourtneygboston@gmail.com>
2022-01-16 09:39:48 -0600

    took unnecessary icons off of cards, styling

--- client/src/components/WeatherCards/index.js
+++ client/src/components/WeatherCards/index.js
@@ -6,14 +6,10 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -40,16 +36,6 @@ export default function WeatherCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        // avatar={
-        //   <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-        //     R
-        //   </Avatar>
-        // }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
         title={data.name}
         subheader={new Date().toLocaleTimeString()}
       />
@@ -61,20 +47,11 @@ export default function WeatherCard(props) {
         alt="Weather GIF"
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body1" color="text.secondary">
           <div>Temp: {data.main.temp} °F </div>
-          <div>Humidity: {data.main.humidity} RH</div>
-          <div>Visibility: {data.visibility} </div>
-          <div>Wind speed: {data.wind.speed} m/s</div>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
@@ -86,9 +63,11 @@ export default function WeatherCard(props) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>More Info:</Typography>
-          <Typography paragraph>
-            TBD DATA
+          <Typography paragraph>More Information:</Typography>
+          <Typography variant="body2" color="text.primary">
+          <div>Humidity: {data.main.humidity} RH</div>
+          <div>Visibility: {data.visibility} </div>
+          <div>Wind speed: {data.wind.speed} m/s</div>
           </Typography>
         </CardContent>
       </Collapse>
