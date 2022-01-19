c683fcfe96cc40e607493682821f7405cdf7771d
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-01-18 18:30:26 -0600

    commit before suzette pull

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,7 +21,7 @@ const resolvers = {
       addUser: async (parent, args) => {
         const user = await User.create(args);
         const token = signToken(user);
-  
+        console.log('user created')
         return { token, user };
       },
       login: async (parent, { email, password }) => {
