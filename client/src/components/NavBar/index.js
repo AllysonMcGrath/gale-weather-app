bc39210d46cd15a19b15037addc1e086e0ebaad5
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-01-20 20:39:14 -0600

    removed unused variables navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,14 +6,10 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
+
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import Auth from '../../utils/auth';
